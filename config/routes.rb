24eac812536c795bcd4586417e6fe1a684d1f0ea
alexv7 <alexvarjabedian7@gmail.com>
2016-03-03 16:20:02 -0500

    setup user model, and created mailbox controller & functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #get 'welcome/index'
+  root 'welcome#index'
+
+  # mailbox folder routes
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+
+  # conversations
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
